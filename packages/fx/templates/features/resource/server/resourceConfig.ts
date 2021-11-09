92723188a9755d5f9cf88802e46f59122167b27f
Andrew Lee <andyrewlee@gmail.com>
2021-11-09 01:25:45 -0800

    Update routes

--- packages/fx/templates/features/resource/server/resourceConfig.ts
+++ packages/fx/templates/features/resource/server/resourceConfig.ts
@@ -1,10 +1,7 @@
 export const resourceRoutes = {
-  signup: "/api/<%= name.toLowerCase() %>/signup",
-  login: "/api/<%= name.toLowerCase() %>/login",
-  logout: "/api/<%= name.toLowerCase() %>/logout",
-  google: "/api/<%= name.toLowerCase() %>/google",
-  googleCallback: "/api/<%= name.toLowerCase() %>/google/callback",
-  facebook: "/api/<%= name.toLowerCase() %>/facebook",
-  facebookCallback: "/api/<%= name.toLowerCase() %>/facebook/callback",
-  refresh: "/api/<%= name.toLowerCase() %>/refresh_token",
+  index: "/api/<%= h.inflection.pluralize(name) %>",
+  create: "/api/<%= h.inflection.pluralize(name) %>",
+  show: "/api/<%= h.inflection.pluralize(name) %>/:id",
+  update: "/api/<%= h.inflection.pluralize(name) %>",
+  destroy: "/api/<%= h.inflection.pluralize(name) %>",
 };
