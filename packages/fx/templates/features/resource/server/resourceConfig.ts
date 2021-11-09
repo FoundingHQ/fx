0da6545f91cdbe1b104e4a29589cae6766cb2246
Andrew Lee <andyrewlee@gmail.com>
2021-11-08 22:36:20 -0800

    Make routes dynamic

---
+++ packages/fx/templates/features/resource/server/resourceConfig.ts
@@ -0,0 +1,10 @@
+export const resourceRoutes = {
+  signup: "/api/<%= name.toLowerCase() %>/signup",
+  login: "/api/<%= name.toLowerCase() %>/login",
+  logout: "/api/<%= name.toLowerCase() %>/logout",
+  google: "/api/<%= name.toLowerCase() %>/google",
+  googleCallback: "/api/<%= name.toLowerCase() %>/google/callback",
+  facebook: "/api/<%= name.toLowerCase() %>/facebook",
+  facebookCallback: "/api/<%= name.toLowerCase() %>/facebook/callback",
+  refresh: "/api/<%= name.toLowerCase() %>/refresh_token",
+};
