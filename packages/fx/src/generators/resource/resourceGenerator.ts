3556160fda9d975f590418dd6cc8c64c9bca092f
Andrew Lee <andyrewlee@gmail.com>
2021-11-09 18:47:50 -0800

    Write to schema file

--- packages/fx/src/generators/resource/resourceGenerator.ts
+++ packages/fx/src/generators/resource/resourceGenerator.ts
@@ -1,6 +1,11 @@
 import fs from "fs";
 import prompts from "prompts";
-import { getSchema } from "@mrleebo/prisma-ast";
+import {
+  getSchema,
+  KeyValue,
+  printSchema,
+  Property,
+} from "@mrleebo/prisma-ast";
 
 import { Generator } from "../../types";
 import { convertProjectPath, convertTemplatePaths } from "../../config";
@@ -48,13 +53,101 @@ export default {
     return [...baseConfig.templates.map(convertTemplatePaths)];
   },
   codemods: async ({ name, attributes }) => {
-    console.log(`Code mods: ${name}, attributes: ${attributes}`);
-
     const schemaPath = convertProjectPath("prisma/schema.prisma");
     const source = fs.readFileSync(schemaPath, "utf8");
     const schema = getSchema(source);
-    console.log("\n\nschema", schema);
-    // TODO: update prisma schema
+
+    const properties: Array<Property> = attributes
+      .split(" ")
+      .map((attribute: string) => {
+        const [fieldName, fieldType] = attribute.split(":");
+        return { type: "field", name: fieldName, fieldType };
+      });
+
+    const primaryKey: any = {
+      type: "field",
+      name: "id",
+      fieldType: "String",
+      array: false,
+      optional: false,
+      attributes: [
+        {
+          type: "attribute",
+          name: "id",
+          kind: "field",
+          group: "",
+        },
+        {
+          type: "attribute",
+          name: "default",
+          kind: "field",
+          args: [
+            {
+              type: "attributeArgument",
+              value: {
+                type: "function",
+                name: "cuid",
+              },
+            },
+          ],
+        },
+      ],
+    };
+
+    const createdAt: any = {
+      type: "field",
+      name: "createdAt",
+      fieldType: "DateTime",
+      array: false,
+      optional: false,
+      attributes: [
+        {
+          type: "attribute",
+          name: "default",
+          kind: "field",
+          args: [
+            {
+              type: "attributeArgument",
+              value: {
+                type: "function",
+                name: "now",
+              },
+            },
+          ],
+        },
+      ],
+    };
+
+    const updatedAt: any = {
+      type: "field",
+      name: "updatedAt",
+      fieldType: "DateTime",
+      array: false,
+      optional: false,
+      attributes: [
+        {
+          type: "attribute",
+          name: "updatedAt",
+          kind: "field",
+        },
+      ],
+    };
+
+    schema.list.push({
+      type: "model",
+      name,
+      properties: [primaryKey, ...properties, createdAt, updatedAt],
+    });
+
+    const newSource = printSchema(schema);
+    fs.writeFile(schemaPath, newSource, (err) => {
+      if (err) {
+        throw {
+          command: "add",
+          message: "Failed to write to schema",
+        };
+      }
+    });
 
     return;
   },
