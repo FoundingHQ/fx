b16841696e70a570a40ea43e48de8692ab45ccd7
Andrew Lee <andyrewlee@gmail.com>
2021-11-11 02:12:39 -0800

    Correctly interpolate in resource api

--- packages/fx/templates/features/api/resource/index.ts
+++ packages/fx/templates/features/api/resource/index.ts
@@ -1,35 +1,35 @@
 import { createHandler } from "@server/handler";
 import {
-  createResource,
-  getResource,
-  getResources,
-  updateResource,
-  destroyResource,
-} from "@lib/auth/server/resourceService";
-import { resourceRoutes } from "@lib/auth/server/resourceConfig";
+  create<%= h.changeCase.pascalCase(name) %>,
+  get<%= h.changeCase.pascalCase(name) %>,
+  get<%= h.changeCase.pascalCase(name) %>List,
+  update<%= h.changeCase.pascalCase(name) %>,
+  delete<%= h.changeCase.pascalCase(name) %>,
+} from "@lib/auth/server/<%= h.changeCase.camelCase(name) %>Service";
+import { <%= h.changeCase.camelCase(name) %>Routes } from "@lib/auth/server/<%= h.changeCase.camelCase(name) %>Config";
 
 const handler = createHandler({ attachParams: true });
 
 handler
-  .get(resourceRoutes.index, async (_req, res) => {
-    const resource = await getResources();
-    res.status(200).json({ resource: resource });
+  .get(<%= h.changeCase.camelCase(name) %>Routes.index, async (_req, res) => {
+    const <%= h.changeCase.camelCase(name) %> = await get<%= h.changeCase.pascalCase(name) %>List();
+    res.status(200).json({ <%= h.changeCase.camelCase(name) %>: <%= h.changeCase.camelCase(name) %> });
   })
-  .post(resourceRoutes.create, async (req, res) => {
-    const newResource = await createResource(req.resource);
-    res.status(200).json({ resource: newResource });
+  .post(<%= h.changeCase.camelCase(name) %>Routes.create, async (req, res) => {
+    const new<%= h.changeCase.camelCase(name) %> = await create<%= h.changeCase.pascalCase(name) %>(req.resource);
+    res.status(200).json({ <%= h.changeCase.camelCase(name) %>: new<%= h.changeCase.camelCase(name) %> });
   })
-  .get(resourceRoutes.show, async (req, res) => {
-    const resource = await getResource(req.resourceId);
-    res.status(200).json({ resource });
+  .get(<%= h.changeCase.camelCase(name) %>Routes.show, async (req, res) => {
+    const <%= h.changeCase.camelCase(name) %> = await get<%= h.changeCase.pascalCase(name) %>(req.resourceId);
+    res.status(200).json({ <%= h.changeCase.camelCase(name) %> });
   })
-  .patch(resourceRoutes.update, async (req, res) => {
-    const updatedResource = await updateResource(req.resourceId, req.attrs);
-    res.status(200).json({ resource: updatedResource });
+  .patch(<%= h.changeCase.camelCase(name) %>Routes.update, async (req, res) => {
+    const updated<%= h.changeCase.camelCase(name) %> = await update<%= h.changeCase.pascalCase(name) %>(req.resourceId, req.attrs);
+    res.status(200).json({ <%= h.changeCase.camelCase(name) %>: updated<%= h.changeCase.camelCase(name) %> });
   })
-  .delete(resourceRoutes.destroy, async (req, res) => {
-    const deletedResource = await destroyResource(req.resourceId);
-    res.status(200).json({ resource: deletedResource });
+  .delete(<%= h.changeCase.camelCase(name) %>Routes.destroy, async (req, res) => {
+    const deleted<%= h.changeCase.camelCase(name) %> = await delete<%= h.changeCase.pascalCase(name) %>(req.resourceId);
+    res.status(200).json({ <%= h.changeCase.camelCase(name) %>: deleted<%= h.changeCase.camelCase(name) %> });
   });
 
 export default handler;
