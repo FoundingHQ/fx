8c752627887fc78c12b98b55d0f44f02ec3d74a7
Kaiyu Hsu <im.kaiyu@gmail.com>
2021-12-04 20:14:32 +0800

    update init generator

--- generators/init[dev]/index.ts
+++ generators/init[dev]/index.ts
@@ -7,10 +7,45 @@ const generator: Generator<Props> = {
     return { ...options };
   },
   async install() {
-    return [];
+    const nextjsDeps = [
+      { name: "@prisma/client" },
+      { name: "cross-fetch" },
+      { name: "next-connect" },
+      { name: "react-query" },
+      { name: "dotenv-cli", isDevDep: true },
+      { name: "prisma", isDevDep: true },
+    ];
+
+    const expoDeps = [
+      { name: "expo" },
+      { name: "@react-navigation/native-stack" },
+      { name: "expo-status-bar" },
+      { name: "react-native" },
+      { name: "react-native-safe-area-context" },
+      { name: "react-native-screens" },
+      { name: "@types/react-native", isDevDep: true },
+    ];
+
+    return nextjsDeps;
   },
   async scaffold() {
-    return [];
+    const nextjsTemplates = [
+      { src: "templates/_app.tsx.ejs", dest: "pages/_app.tsx" },
+      { src: "templates/.env.ejs", dest: ".env" },
+      { src: "templates/docker-compose.yml.ejs", dest: "docker-compose.yml" },
+      {
+        src: "templates/trustProxy.ts.ejs",
+        dest: "lib/core/server/middlewares/trustProxy.ts",
+      },
+      { src: "templates/handler.ts.ejs", dest: "lib/core/server/handler.ts" },
+      { src: "templates/prisma.ts.ejs", dest: "lib/core/server/prisma.ts" },
+      { src: "templates/schema.prisma.ejs", dest: "db/schema.prisma" },
+      { src: "templates/seed.ts.ejs", dest: "db/seed.ts" },
+    ];
+
+    const expoTemplates = [];
+
+    return nextjsTemplates;
   },
   async codemods() {
     return [];
